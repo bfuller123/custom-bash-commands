ec2fec9ff47bc59cc13b164737ea4885432e55f9
Brett Fuller <bfuller123@gmail.com>
2021-10-08 16:39:35 -0500

    add download pages

--- laptopsetup.sh
+++ laptopsetup.sh
@@ -14,3 +14,11 @@ echo "npm install -g @angular/cli"
 # git cred manager
 echo "brew tap microsoft/git"
 echo "brew install --cask git-credential-manager-core"
+
+# app download pages
+open https://docs.docker.com/desktop/mac/install
+open https://visualstudio.microsoft.com/vs/mac/
+open https://docs.microsoft.com/en-us/sql/azure-data-studio/download-azure-data-studio
+open https://code.visualstudio.com
+open https://www.postman.com/downloads
+open https://www.google.com/search?q=github+ssh+key+setup
